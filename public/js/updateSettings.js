d27df593e5945facc18043bfb643846822b3600b
Subhash Chandra <chandra.subhash61@gmail.com>
2020-11-22 23:46:20 +0530

    password change functinal

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -2,18 +2,21 @@ import axios from 'axios';
 
 import { showAlert } from './alert';
 
-export const updateSettings = async (name, email) => {
+//type is either 'password or 'data'
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://127.0.0.1:8000/api/v1/users/updateMyPassword'
+        : 'http://127.0.0.1:8000/api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:8000/api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url,
+      data,
     });
     if (res.data.status === 'success') {
-      showAlert('success', 'Data Updated Successfully');
+      showAlert('success', `${type.toUpperCase()} Updated Successfully`);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
