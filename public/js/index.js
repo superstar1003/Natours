2178b58519676463dd9b5a67b87c183ed9524e3b
Subhash Chandra <chandra.subhash61@gmail.com>
2020-11-23 11:45:06 +0530

    correted the updating tab

--- public/js/index.js
+++ public/js/index.js
@@ -27,7 +27,7 @@ if (loginForm) {
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
-if (userDataForm) 
+if (userDataForm)
   userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const name = document.getElementById('name').value;
@@ -35,19 +35,21 @@ if (userDataForm)
     updateSettings({ name, email }, 'data');
   });
 
-if (userPasswordForm) 
-  userPasswordForm.addEventListener('submit', (e) => {
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     document.querySelector('.btn--save-password').textContent = 'Updating...';
-    
+
     const passwordCurrent = document.getElementById('password-current').value;
     const password = document.getElementById('password').value;
     const passwordConfirm = document.getElementById('password-confirm').value;
-    await updateSettings({ passwordCurrent, password, passwordConfirm }, 'password');
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
 
     document.querySelector('.btn--save-password').textContent = 'Save password';
     document.getElementById('password-current').value = '';
     document.getElementById('password').value = '';
     document.getElementById('password-confirm').value = '';
   });
-
--- public/js/login.js
+++ public/js/login.js
@@ -30,7 +30,9 @@ export const logout = async () => {
       method: 'GET',
       url: 'http://127.0.0.1:8000/api/v1/users/logout',
     });
-    if (res.data.status === 'success') location.reload(true);
+    if (res.data.status === 'success') {
+      location.assign('/login');
+    }
   } catch (err) {
     console.log(err.response);
     showAlert('error', 'Error logging out. Try again');
