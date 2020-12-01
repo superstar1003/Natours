017179430e8f53e41f5249e562816fb8163a8b0a
Subhash Chandra <chandra.subhash61@gmail.com>
2020-12-01 13:54:08 +0530

    improving strpe checkout -9

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,7 +4,7 @@ const authController = require('./../controllers/authController');
 const router = express.Router();
 
 const viewController = require('../controllers/viewController');
-const bookingController = require('../controllers/bookingController');
+//const bookingController = require('../controllers/bookingController');
 
 router.get('/', authController.isLoggedIn, viewController.getOverview);
 router.get(`/tour/:slug`, authController.isLoggedIn, viewController.getTour);
