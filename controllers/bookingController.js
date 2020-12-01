1b2f2d71b74c945da20e9708b8190d126fae4be6
Subhash Chandra <chandra.subhash61@gmail.com>
2020-12-01 11:10:41 +0530

    improving strpe checkout -5

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -51,7 +51,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
 
   await Booking.create({ tour, user, price });
 };
@@ -60,7 +60,7 @@ exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
   let event;
   try {
-    const event = stripe.webhooks.constructEvent(
+    event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
