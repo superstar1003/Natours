c29a65601be7c37fbe10246064a03ab353477aff
Subhash Chandra <chandra.subhash61@gmail.com>
2020-12-01 14:45:03 +0530

    Implemented stripe message-1

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,10 +3,10 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.use(viewController.alerts);
-
 const viewController = require('../controllers/viewController');
 //const bookingController = require('../controllers/bookingController');
+
+router.use(viewController.alerts);
 
 router.get('/', authController.isLoggedIn, viewController.getOverview);
 router.get(`/tour/:slug`, authController.isLoggedIn, viewController.getTour);
